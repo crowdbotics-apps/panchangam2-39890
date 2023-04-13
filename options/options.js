ef1524ff2191ba318c01f1e24f55a97e3b61fd34
Crowdbotics <team@crowdbotics.com>
2023-04-13 04:02:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "panchangam2-39890",
+  url: "https://panchangam2-39890.botics.co",
+  api: "https://panchangam2-39890.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
